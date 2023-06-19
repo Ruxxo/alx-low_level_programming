da684816ca2b04761b0f821bb843c92edd39c17c
Ruxxo <ebukaogd1@gmail.com>
2023-06-19 09:35:21 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+	{
+	char letter;
+
+	for
+		(letter  = 'a', a < = 'z', a++);
+	putchar(a);
+	putchar(\n);
+
+	return (0);
+	}
+
