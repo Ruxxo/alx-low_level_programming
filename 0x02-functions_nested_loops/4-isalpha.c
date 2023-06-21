612b63cb820d1015e2655eaea2e55e64a3a92ef8
Ruxxo <ebukaogd1@gmail.com>
2023-06-21 09:03:43 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - Function that checks for alphabetic character
+ * @c: Character in ASCII code
+ * Return: 1 if it is an alphabet, 0 for others
+ */
+
+int _isalpha(int c)
+{
+	if
+		((c >= 65 && c <= 90) || if (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+		return (0);
+}
