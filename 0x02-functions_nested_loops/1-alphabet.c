f5514f2c44a4342753a928abc4868ffb0bd94cab
Ruxxo <ebukaogd1@gmail.com>
2023-06-21 02:31:40 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description: Print all the alphabets in lower case
+ * Return: Invariably 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+
+	{
+		_putchar(x);
+		x++;
+	}
+		_putchar('\n');
+
+	Return(0);
+}
+
